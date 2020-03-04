543228489ccdf7bc788a39a68e1fd1e9938bba76
Cláudio Monteiro <clam@cin.ufpe.br>
2020-03-04 15:36:07 -0300

    variavel-dependente

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -46,5 +46,31 @@ for i in dic_init:
     pais.append(i)
     data_primeiro_proj.append(dic_init[i])
 
-projeto_init = pd.DataFrame(pais, data_primeiro_proj)
+projeto_init = pd.DataFrame(pais, data_primeiro_proj).reset_index()
+projeto_init.columns = ['data_inicio', 'pais']
 
+# criar variavel ano
+projeto_init['ano_inicio'] = projeto_init['data_inicio'].map(lambda x: x.year )
+
+# criar dataframe com anos ate data de inicio do primeiro projeto
+dataset = pd.DataFrame(columns=['pais', 'anos', 'inicio_projeto'])
+
+for i in range(len(projeto_init)):
+    # criar listas de cada pais
+    anos = list(range(1999,int( str(projeto_init['ano_inicio'][i]))+1 ))
+    pais = (( projeto_init['pais'][i]+';') * len(anos)).split(';')[:-1]
+    interv = list([0]*(len(anos)-1))
+    interv.append(1)
+
+    # criar dataframe e adicionar 
+    dt_pais = pd.DataFrame(
+        {'pais': pais,
+        'anos': anos,
+        'inicio_projeto': interv 
+        })
+
+    dataset = pd.concat([dataset, dt_pais], )
+
+
+# salvar
+dataset.to_csv('resultados/DATASET.csv')
