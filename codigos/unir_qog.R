46ce8e1677c74ee19297f189c7baace11f5c7ef9
georgiaribeiro <georgiamribeiro@gmail.com>
2020-04-01 21:00:16 -0300

    commit_geo ~01.04

--- codigos/unir_qog.R
+++ codigos/unir_qog.R
@@ -8,9 +8,10 @@
 
 #Carregar pacote
 library(tidyverse)
+library(dplyr)
 
 #Carregar bancos
-DATASET = read.csv("resultados/DATASET.csv")
+DATASET = read.csv("resultados/DATASET_pt-en.csv", sep=";")
 QOG = read.csv("dados/qog_bas_ts_jan20.csv")
 
 #selecionar variaveis escolhidas do QOG
@@ -20,6 +21,13 @@ QOG = select(QOG, cname, year, icrg_qog, ht_region)
 QOG = QOG %>% filter((between(year, 1999,2010)),
                      (between(ht_region, 3,4)))
 
+#ajustar categoria da coluna
+QOG$cname = as.character(QOG$cname)
+QOG$cname = as.factor(QOG$cname)
+
 #Juntar bancos
-left_join(DATASET, QOG %>% select(cname, icrg_qog), 
-                    by = c("cname" = "paises"))
+DATASET_qog = left_join(DATASET, QOG %>% select(cname, icrg_qog, year), 
+                    by = c("cname", "anos" = "year"))
+
+#salvar
+write.csv(DATASET_qog,"resultados/DATASET_QOG.csv", row.names = FALSE)
