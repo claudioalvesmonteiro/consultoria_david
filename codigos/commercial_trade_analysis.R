e4a3556c259282bc06dd6340e2afb992e5ead2de
Georgia Ribeiro <georgiamribeiro@gmail.com>
2020-07-07 21:20:55 -0300

    07/07

--- codigos/commercial_trade_analysis.R
+++ codigos/commercial_trade_analysis.R
@@ -33,6 +33,7 @@ PALOP = c("Cape Town","Guinea-Bissau","Equatorial Guinea","Sao Tome and Principe
 #Variável 1 - % de comercio
 #checar nome dos parceiros 
 t = table(df$Partner)
+rm(t)
 
 a1 = df %>% filter(!grepl(", nes",Partner)) %>% #excluir agrupados por continente
   group_by(Year, Reporter, Partner) %>% summarise(Freq=n())
@@ -55,7 +56,7 @@ df2 = read_excel("dados/projetos_brasil.xlsm")
 
 df2$ano_inicio = str_sub(df2$Início, end=4) #data completa -> somente ano
 
-#Separar paises (ver obs 30)
+#Separar paises (ver obs. 30)
 df2$País = gsub("\\; ", ",", df2$País)
 df2=df2 %>% 
   mutate(País=strsplit(País, ",")) %>% 
@@ -64,22 +65,76 @@ df2=df2 %>%
 #calcular numero de projetos
 df2 = df2 %>% select(ano_inicio, País) %>%
   filter(País %in% c("Etiópia", "Quênia", "Nigéria", "África do Sul", "Tanzânia")) %>%
-  group_by(ano_inicio, País) %>%
-  summarise(num_projetos=n())
+  group_by(ano_inicio, País) %>% summarise(num_projetos=n())
   
-#traduzir nomes
+#para unir bancos
+#1. traduzir nomes
 a1$Reporter = mapvalues(a1$Reporter, c("Ethiopia", "Kenya", "Nigeria", "South Africa", "United Rep. of Tanzania"),
                                       c("Etiópia", "Quênia", "Nigéria", "África do Sul", "Tanzânia"))
-df2$Reporter = df2$País
+#2. renomear colunas
+colnames(df2) = c("Year", "Reporter", "num_projetos")
+#3. transformar anos em numerico
+df2$Year = as.numeric(df2$Year)
+df_all = full_join(a1[,-c(3:4)], df2, by = c('Year','Reporter')) #unir
+
+df_all = gather(df_all, "Variavel", "Valor", 3:4) #formato tidy
 
 #visualizar
-ggplot() + 
-  geom_line(data = a1, aes(x=Year, y=perc_palop), color='#2D93AD', size=1) + 
-  geom_line(data = df2,aes(x=as.numeric(ano_inicio), y=num_projetos), color='#DE8F6E', size=1) +
-  geom_point(data = df2,aes(x=as.numeric(ano_inicio), y=num_projetos), color='#DE8F6E', size = 2) +
-  facet_wrap(~Reporter) +
-  scale_x_continuous(breaks = seq(1992, 2017, by = 5)) +
-  labs(x = "", y = "") +
-  theme_minimal()  +
-  theme(         panel.border = element_rect(fill = NA, color = "#E6E6E6", size = 1.25, linetype = "solid"),
-                 axis.ticks = element_line(colour = '#E6E6E6', size = 1, linetype = 'solid'))
+ggplot(df_all, aes(Year, Valor, group=Variavel)) + 
+  geom_line(aes(colour=Variavel)) +
+  geom_point(data = pick(~Variavel == "num_projetos"), colour = "#DE8F6E", size=0.8)+
+  facet_wrap(~Reporter, scales = 'free_x') +
+  scale_x_continuous(breaks = seq(1992, 2017, by = 3)) + ylim(0,10) +
+  scale_color_manual(labels = c("Número de projeto com o Brasil",
+                                "% de comércio com PALOP do total"),
+                     values = c("#DE8F6E", "#2D93AD")) +
+  labs (x="", y="", colour="") +
+  theme_minimal() + theme(axis.text.x = element_text(size=8), legend.position="bottom",
+                         strip.background = element_rect(color="#E6E6E6", fill="white", size=1))+
+  ggsave("graf1.png", path = "resultados", width = 7, height = 4, units = "in")
+                      
+  
+#======================================#
+# Analise 2:  mesma análise 1 com log  #
+#======================================#
+
+#visualizar
+ggplot(df_all, aes(Year, log(Valor), group=Variavel)) + 
+  geom_line(aes(colour=Variavel)) +
+  facet_wrap(~Reporter, scales = 'free_x') +
+  scale_x_continuous(breaks = seq(1992, 2017, by = 3)) + 
+  geom_point(data = pick(~Variavel == "num_projetos"), colour = "#DE8F6E", size=0.8)+
+  scale_color_manual(labels = c("Número de projeto com o Brasil (log)",
+                                "% de comércio com PALOP do total (log)"),
+                     values = c("#DE8F6E", "#2D93AD")) +
+  labs (x="", y="", colour="") +
+  theme_minimal() + theme(axis.text.x = element_text(size=8), legend.position="bottom",
+                        strip.background = element_rect(color="#E6E6E6", fill="white", size=1)) +
+  ggsave("graf2.png", path = "resultados", width = 7, height = 4, units = "in")
+
+#======================================================================#
+# Analise 3: ~Teste de correlação de Pearson, % de comércio com PALOP  #
+# do total X número de projetos de cooperação com o Brasil, por país.  #
+#======================================================================#
+
+#banco
+df3 = full_join(a1[,-c(3:4)], df2, by = c('Year','Reporter'))
+
+cor.test(df3$perc_palop, df3$num_projetos)
+
+
+
+
+#rascunho
+projetos_br = read_excel("dados/projetos_brasil.xlsm")
+projetos_br = filter(projetos_br, Região == "África")
+
+#Separar paises (ver obs. 30)
+projetos_br$País = gsub("\\; ", ",", projetos_br$País)
+projetos_br=projetos_br %>% 
+  mutate(País=strsplit(País, ",")) %>% 
+  unnest(País)
+
+projetos_br$ano_inicio = str_sub(projetos_br$Início, end=4)
+projetos_br = projetos_br %>% select(ano_inicio, País) %>%
+  group_by(ano_inicio, País) %>% summarise(num_projetos=n())
